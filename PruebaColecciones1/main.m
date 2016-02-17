56a56edba0496b1c04d35dc1ca0d376f2aeee721
Akixe <akixe.otegi@gmail.com>
2016-02-17 14:57:44 +0100

    pruebas

--- PruebaColecciones1/main.m
+++ PruebaColecciones1/main.m
@@ -10,8 +10,29 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSArray *lista = [NSArray arrayWithObjects:[NSNumber numberWithFloat:3.141592],[NSDate date], nil];
+        
+        NSArray *nuevoArray = @[@"hola mundo", @2334, @3.141516];
+        
+        for (int i = 0; i < [nuevoArray count]; i++){
+            NSLog(@"El objeto en %d es %@", i, [nuevoArray objectAtIndex:i]);
+        }
+        
+        for (id each in lista){
+            NSLog(@"%@", each);
+        }
+        
+        NSMutableArray *mutable = [nuevoArray mutableCopy];
+        
+        [mutable addObject:@42];
+        
+        [mutable insertObject:@98 atIndex:2];
+        
+        [mutable removeLastObject];
+        
+        NSLog(@"Resultado: %@", mutable);
     }
+    
+    
     return 0;
 }
